dfd03967c4aa421c9c4713ab98737b724893002d
Mahmoud Ghanem <moghanem345@gmail.com>
2023-05-31 16:02:37 +0300

    First Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DCOTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Contact Page</title>
+</head>
+
+<body>
+<h2>This is about page.</h2>
+<a href="{{ route('contact.page') }}">Contact</a>
+</body>
+</html>
+
